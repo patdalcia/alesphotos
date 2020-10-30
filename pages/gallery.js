5cd2588b5063c2a4991d610663daf39fb1c09e63
patdalcia <49659510+patdalcia@users.noreply.github.com>
2020-10-30 16:38:50 -0700

    Updating font-display in css modules in correspondance with lighthouse analytics

--- pages/gallery.js
+++ pages/gallery.js
@@ -15,67 +15,67 @@ export default function Gallery() {
         <Header />
         <div className={styles.main}>
           <div className={styles.card}>
-          <Image className={styles.gImages} src="/galleryPic_1.JPG" width={800} height={530} quality={100}/>
+          <Image className={styles.gImages} alt="" src="/galleryPic_1.JPG" width={800} height={530} quality={100}/>
           </div>
 
           <div className={styles.card}>
-          <Image className={styles.gImages} src="/galleryPic_2.jpeg" width={800} height={530} quality={100}/>
-          </div>
-
-          <div className={styles.break}/>
-
-          <div className={styles.card}>
-          <Image className={styles.gImages} src="/galleryPic_3.JPG" width={800} height={530} quality={100}/>
-          </div>
-
-          <div className={styles.card}>
-          <Image className={styles.gImages} src="/galleryPic_4.JPG" width={800} height={530} quality={100}/>
+          <Image className={styles.gImages} alt="" src="/galleryPic_2.jpeg" width={800} height={530} quality={100}/>
           </div>
 
           <div className={styles.break}/>
 
           <div className={styles.card}>
-          <Image className={styles.gImages} src="/galleryPic_5.JPG" width={800} height={530} quality={100}/>
+          <Image className={styles.gImages} alt="" src="/galleryPic_3.JPG" width={800} height={530} quality={100}/>
           </div>
 
           <div className={styles.card}>
-          <Image className={styles.gImages} src="/galleryPic_6.JPG" width={800} height={530} quality={100}/>
-          </div>
-
-          <div className={styles.break}/>
-
-          <div className={styles.card}>
-          <Image className={styles.gImages} src="/galleryPic_7.JPG" width={800} height={530} quality={100}/>
-          </div>
-
-          <div className={styles.card}>
-          <Image className={styles.gImages} src="/galleryPic_8.JPG" width={800} height={530} quality={100}/>
+          <Image className={styles.gImages} alt="" src="/galleryPic_4.JPG" width={800} height={530} quality={100}/>
           </div>
 
           <div className={styles.break}/>
 
           <div className={styles.card}>
-          <Image className={styles.gImages} src="/galleryPic_9.JPG" width={800} height={530} quality={100}/> 
+          <Image className={styles.gImages} alt="" src="/galleryPic_5.JPG" width={800} height={530} quality={100}/>
+          </div>
+
+          <div className={styles.card}>
+          <Image className={styles.gImages} alt="" src="/galleryPic_6.JPG" width={800} height={530} quality={100}/>
+          </div>
+
+          <div className={styles.break}/>
+
+          <div className={styles.card}>
+          <Image className={styles.gImages} alt="" src="/galleryPic_7.JPG" width={800} height={530} quality={100}/>
+          </div>
+
+          <div className={styles.card}>
+          <Image className={styles.gImages} alt="" src="/galleryPic_8.JPG" width={800} height={530} quality={100}/>
+          </div>
+
+          <div className={styles.break}/>
+
+          <div className={styles.card}>
+          <Image className={styles.gImages} alt="" src="/galleryPic_9.JPG" width={800} height={530} quality={100}/> 
           </div>
           
           <div className={styles.card}>
-          <Image className={styles.gImages} src="/galleryPic_10.JPG" width={800} height={530} quality={100}/>
+          <Image className={styles.gImages} alt="" src="/galleryPic_10.JPG" width={800} height={530} quality={100}/>
           </div>
 
           <div className={styles.break}/>
 
           <div className= {styles.card}>
-          <Image className={styles.gImages} src="/galleryPic_11.JPG" width={800} height={530} quality={100}/>
+          <Image className={styles.gImages} alt="" src="/galleryPic_11.JPG" width={800} height={530} quality={100}/>
           </div>
 
           <div className= {styles.card}>
-          <Image className={styles.gImages} src="/galleryPic_12.JPG" width={800} height={530} quality={100}/>
+          <Image className={styles.gImages} alt="" src="/galleryPic_12.JPG" width={800} height={530} quality={100}/>
           </div>
 
           <div className={styles.break}/>
 
           <div className= {styles.card}>
-          <Image className={styles.gImages} src="/galleryPic_13.JPG" width={800} height={530} quality={100}/>
+          <Image className={styles.gImages} alt="" src="/galleryPic_13.JPG" width={800} height={530} quality={100}/>
           </div>
 
         </div> 
