649c869c2bb61d7cba5dc36caa7092e93dab8967
patdalcia <49659510+patdalcia@users.noreply.github.com>
2020-10-30 17:18:12 -0700

    Optimize footer instagram icon in accordance with lighthouse analytics

--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,12 @@
 import styles from "../styles/Footer.module.css";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <footer className={styles.footer}>
       
       <a href="https://www.instagram.com/_alesphotos/">
-        <img src="/instagram_Icon.png" alt="Instagram Icon"/>
+        <Image className={styles.gImages} alt="" src="/instagram_Icon.png" width={24} height={24} quality={100}/>
       </a>
     
     </footer>
